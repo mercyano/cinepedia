11fc133b929104608d3566cc5b674a0862c613e6
Mercyano Dandi Hidayat <mercyanodandi0@gmail.com>
2023-05-17 23:56:21 +0700

    Basic routing

---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,13 @@
+// import MovieCard from '../components/MovieCard';
+import Navbar from '../components/Navbar';
+
+const MovieDetail = () => {
+  return (
+    <>
+      <Navbar />
+      {/* <MovieCard /> */}
+    </>
+  );
+};
+
+export default MovieDetail;
--- src/pages/MovieDetailsPage.jsx
+++
@@ -1,5 +0,0 @@
-const MovieDetailsPage = () => {
-  return <div>MovieDetailsPage</div>;
-};
-
-export default MovieDetailsPage;
