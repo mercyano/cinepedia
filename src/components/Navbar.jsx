f1b22ca897f9c927742557dbc53bb340ef31ce6b
Mercyano Dandi Hidayat <mercyanodandi0@gmail.com>
2023-05-20 00:39:42 +0700

    add search component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { FaGithub } from 'react-icons/fa';
 
 import logo from '../assets/cinepedia_logo.svg';
+import SearchForm from './SearchForm';
 
 const Navbar = () => {
   return (
@@ -11,21 +12,25 @@ const Navbar = () => {
         <div className="flex items-center justify-between">
           {/* Logo */}
           <div className="w-32 z-10 md:w-48">
-            <a href="#">
+            <a href="/">
               <img src={logo} alt="logo" />
             </a>
           </div>
 
           {/* Menu Items */}
-          <div className="flex items-center space-x-10">
+          <div className="flex items-center ml-4 space-x-4 z-10">
+            {/* Search Form */}
+            <div className="z-10">
+              <SearchForm />
+            </div>
             {/* GitHub Link */}
             <a
               href="https://github.com/mercyano"
               target="_blank"
               rel="noopener noreferrer"
-              className="z-10 ml-4 text-white hover:text-gray-300"
+              className=" ml-4 text-white hover:text-gray-300"
             >
-              <FaGithub size={24} />
+              <FaGithub size={28} />
             </a>
           </div>
         </div>
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,5 +1,22 @@
+// import { AiOutlineSearch } from 'react-icons/ai';
+
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  return (
+    <div className="relative flex items-center focus-within:text-gray-500">
+      <input
+        type="search"
+        className="h-6 text-black rounded-md placeholder-slate-400 focus:border-yellow-500 focus:ring-1 focus:ring-yellow-500 md:w-full md:h-full"
+        placeholder="Search movies..."
+        name="searchMovie"
+        id="searchMovie"
+      />
+
+      {/* <AiOutlineSearch
+        className="absolute right-2 pointer-events-none text-slate-300"
+        size={20}
+      /> */}
+    </div>
+  );
 };
 
 export default SearchForm;
