1e6cd76b3d59afa5bcdfd54a791a843ec337a8d4
Mercyano Dandi Hidayat <mercyanodandi0@gmail.com>
2023-05-21 19:06:21 +0700

    production mode

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
+  mode: 'production',
+  build: {
+    envFile: '.env.production',
   },
 });
